523b8bea77e66b40a726fa63f5dfb4d28a0711a4
Anastasios Georgakopoulos <georgako@gmail.com>
2018-09-17 16:11:07 +0300

    first commit

---
+++ en/contact.php
@@ -0,0 +1,262 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Rooms to let Antonis - Contact with us</title>
+<link href="../css/sifnos.css" rel="stylesheet" type="text/css" />
+<link href="../css/menu.css" rel="stylesheet" type="text/css" />
+<script src="../SpryAssets/SpryValidationTextField.js" type="text/javascript"></script>
+<link href="../SpryAssets/SpryValidationTextField.css" rel="stylesheet" type="text/css" />
+<script src='https://www.google.com/recaptcha/api.js?hl=en-GB'></script>
+</head>
+
+<body>
+<!--[if lte IE 6]>
+	<script type="text/javascript" src="supersleight-min.js"></script>
+<![endif]-->
+
+<div id="container">
+<div id="topImg">
+<img src="../images/exterior-spaces.jpg" width="540" height="250" alt="Exterior spaces of Antonis rooms in Kastro" />
+</div>
+<div id="menu">
+  <table width="100%" border="0" cellspacing="0" cellpadding="0">
+    <tr valign="top">
+      <td class="menu"><a href="index.html">Home</a></td>
+      </tr>
+    <tr valign="top">
+      <td class="menu"><a href="rooms.html">The rooms</a></td>
+      </tr>
+    <tr valign="top">
+      <td class="menu"><a href="rates.html">Rates</a></td>
+      </tr>
+    <tr valign="top">
+      <td class="menu"><b>Contact</b></td>
+      </tr>
+    <tr valign="top">
+      <td class="menu"><a href="location.html">Location</a></td>
+      </tr>
+    <tr valign="top">
+      <td class="menu"><a href="kastro.html">Kastro</a></td>
+      </tr>
+      <tr valign="middle">
+      <td align="center"><br />
+        <a href="index.html"><img src="../images/flags/English.gif" width="16" height="11" alt="Rooms Antonis in Sifnos - English" /> </a> <a href="../fr/index.html"><img src="../images/flags/french.gif" width="16" height="11" alt="Chambres Antonis Sifnos - français" /> </a> <a href="../index.html"><img src="../images/flags/greek.gif" width="16" height="11" alt="Δωμάτια Αντώνης στη Σίφνο - Ελληνικά" /></a></td>
+      </tr>
+    </table>
+  </div>
+  <div id="mainContent">
+    <h1>Contact &amp; Reservations</h1>
+    <table width="100%" summary="Στοιχεία επικοινωνίας με τα δωμάτια Αντώνης">
+      <tr>
+        <td width="39%"><p>For more information and reservations you can contact us by telephone or send us the following form:</p></td>
+        <td width="31%"><div id="contact">
+          <p>Antonis Atsonios<br />
+            Kastro - Sifnos<br />
+            Cyclades
+            </p>
+          </div>
+        </td>
+        <td width="30%"><div id="contact">
+          <p>Tel.: +30 22840 33708<br />
+            Mob.: +30 6947 021 343<br />
+            <img src="../images/zak.png" width="172" height="14" align="absmiddle" /></p>
+        </div>
+        </td>
+      </tr>
+    </table>
+    <form action="http://www.sifnosholidays.gr/HDWForm2Mail/Form2MailNoCaptcha.php" method="post" name="form1" id="form1" accept-charset="utf-8">
+      <fieldset>
+        <legend>Sifnos - Antonis rooms</legend>
+        <span id="sprytextfield1">
+          <label for="name">Full name:</label>
+          <input name="name" type="text" id="name" size="30" />
+          <span class="textfieldRequiredMsg">Please fill in your name</span><span class="textfieldMinCharsMsg">Please fill in your name</span></span> <br />
+        <span id="sprytextfield2">
+          <label for="email">Email:</label>
+          <input name="email" type="text" id="email" size="30" />
+          <span class="textfieldRequiredMsg">Please fill in your email address</span><span class="textfieldInvalidFormatMsg">Invalid format.</span></span> <br />
+        <span id="sprytextfield3">
+          <label for="phone">Telephone:</label>
+          <input name="phone" type="text" id="phone" size="30" />
+          <span class="textfieldRequiredMsg">Please fill in your telephone number</span><span class="textfieldMinCharsMsg">Minimum number of characters not met.</span></span> <br />
+        <input name="separator1" type="hidden" value="separator1" />
+        Have you ever stayed in our unit before?
+  <input type="radio" name="revisit" id="yes" value="yes" />
+  <label for="yes">Yes</label>
+  <input type="radio" name="revisit" id="no" value="no" />
+  <label for="no">No</label>
+  <br />
+  <label for="wish">I would like:</label>
+  <select name="wish" id="wish">
+    <option>please choose</option>
+    <option value="availability">to ask the availability</option>
+    <option value="information">more information</option>
+    <option value="reservation">to make a reservation</option>
+  </select>
+  <br />
+  <label for="roomType">Accommodation type:</label>
+  <select name="roomType" id="roomType">
+    <option value="2 bed room">Two-bedded room</option>
+    <option value="3 bed room">Three-bedded room</option>
+    <option value="AnyAvailable">Anything available</option>
+  </select>
+  <br />
+  <label for="persons">Number of persons:</label>
+  <select name="persons" id="persons">
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+    <option value="4">4</option>
+    <option value="5">5</option>
+    <option value="6">6</option>
+    <option value="7">7</option>
+    <option value="8">8</option>
+    <option value="9">9</option>
+    <option value="group">Group</option>
+  </select>
+  <input name="separator2" type="hidden" value="separator2" />
+  <br />
+  <label for="arrival_day">Arrival date:</label>
+  <select name="arrival_day" id="arrival_day">
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+    <option value="4">4</option>
+    <option value="5">5</option>
+    <option value="6">6</option>
+    <option value="7">7</option>
+    <option value="8">8</option>
+    <option value="9">9</option>
+    <option value="10">10</option>
+    <option value="11">11</option>
+    <option value="12">12</option>
+    <option value="13">13</option>
+    <option value="14">14</option>
+    <option value="15">15</option>
+    <option value="16">16</option>
+    <option value="17">17</option>
+    <option value="18">18</option>
+    <option value="19">19</option>
+    <option value="20">20</option>
+    <option value="21">21</option>
+    <option value="22">22</option>
+    <option value="23">23</option>
+    <option value="24">24</option>
+    <option value="25">25</option>
+    <option value="26">26</option>
+    <option value="27">27</option>
+    <option value="28">28</option>
+    <option value="29">29</option>
+    <option value="30">30</option>
+    <option value="31">31</option>
+  </select>
+  <select name="arrival_month" id="arrival_month">
+    <option value="Ιανουαρίου">January</option>
+    <option value="Φεβρουαρίου">February</option>
+    <option value="Μαρτίου">March</option>
+    <option value="Απριλίου">April</option>
+    <option value="Μαΐου">May</option>
+    <option value="Ιουνίου">June</option>
+    <option value="Ιουλίου">July</option>
+    <option value="Αυγούστου">August</option>
+    <option value="Σεπτεμβρίου">September</option>
+    <option value="Οκτωβρίου">October</option>
+    <option value="Νοεμβρίου">November</option>
+    <option value="Δεκεμβρίου">December</option>
+  </select>
+  <br />
+  <label for="departure_day">Departure date:</label>
+  <select name="departure_day" id="departure_day">
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+    <option value="4">4</option>
+    <option value="5">5</option>
+    <option value="6">6</option>
+    <option value="7">7</option>
+    <option value="8">8</option>
+    <option value="9">9</option>
+    <option value="10">10</option>
+    <option value="11">11</option>
+    <option value="12">12</option>
+    <option value="13">13</option>
+    <option value="14">14</option>
+    <option value="15">15</option>
+    <option value="16">16</option>
+    <option value="17">17</option>
+    <option value="18">18</option>
+    <option value="19">19</option>
+    <option value="20">20</option>
+    <option value="21">21</option>
+    <option value="22">22</option>
+    <option value="23">23</option>
+    <option value="24">24</option>
+    <option value="25">25</option>
+    <option value="26">26</option>
+    <option value="27">27</option>
+    <option value="28">28</option>
+    <option value="29">29</option>
+    <option value="30">30</option>
+    <option value="31">31</option>
+  </select>
+  <select name="departure_month" id="departure_month">
+    <option value="Ιανουαρίου">January</option>
+    <option value="Φεβρουαρίου">February</option>
+    <option value="Μαρτίου">March</option>
+    <option value="Απριλίου">April</option>
+    <option value="Μαΐου">May</option>
+    <option value="Ιουνίου">June</option>
+    <option value="Ιουλίου">July</option>
+    <option value="Αυγούστου">August</option>
+    <option value="Σεπτεμβρίου">September</option>
+    <option value="Οκτωβρίου">October</option>
+    <option value="Νοεμβρίου">November</option>
+    <option value="Δεκεμβρίου">December</option>
+  </select>
+  <br />
+  <label for="stays">Number of stays:</label>
+  <input name="stays" type="text" id="stays" size="5" />
+  <br />
+  <input name="separator3" type="hidden" value="separator3" />
+  <label for="comments">Comments:</label>
+  <textarea name="comments" cols="40" rows="5" id="comments"></textarea>
+  <br />
+  <div class="captcha" align="right">
+                            <p style="text-align:right">Please click in the box (required):</p>
+                            <div class="g-recaptcha" data-sitekey="6LfjTBcUAAAAAIUB3g0uuHuzPirlo_7231FbPL6A"></div>
+                            <script>
+                                          var onSuccess = function(response) {
+                                            var errorDivs = document.getElementsByClassName("recaptcha-error");
+                                            if (errorDivs.length) {
+                                              errorDivs[0].className = "";
+                                            }
+                                            var errorMsgs = document.getElementsByClassName("recaptcha-error-message");
+                                            if (errorMsgs.length) {
+                                              errorMsgs[0].parentNode.removeChild(errorMsgs[0]);
+                                            }
+                                          };
+                                        </script> 
+                            <!-- END: ReCAPTCHA --></div>
+   <input type="submit" name="submit" id="submit" value="Send form" />
+   <input type="reset" name="reset" id="reset" value="Reset fields" />
+  <br />
+      </fieldset>
+      <input type="hidden" name="hdwemail" id="hdwemail" value="" />
+      <input type="hidden" name="hdwok" id="hdwok" value="confirmation-en.html" />
+      <input type="hidden" name="hdwnook" id="hdwnook" value="error-en.html" />
+    </form>
+  </div>
+<div id="footer">
+&copy; Antonis Rooms 2011 | <a href="http://lysiteleia.gr" target="_blank" class="lysiteleia">Creation - Hosting: Lysiteleia</a>
+</div>
+<!-- end #container --></div>
+<script type="text/javascript">
+<!--
+var sprytextfield1 = new Spry.Widget.ValidationTextField("sprytextfield1", "none", {validateOn:["blur"], minChars:3});
+var sprytextfield2 = new Spry.Widget.ValidationTextField("sprytextfield2", "email", {validateOn:["blur"]});
+var sprytextfield3 = new Spry.Widget.ValidationTextField("sprytextfield3", "none", {validateOn:["blur"], minChars:10});
+//-->
+</script>
+</body>
+</html>
